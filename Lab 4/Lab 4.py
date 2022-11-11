73f49a1d8107d11eb1c9bea4a918f418ac388a03
DESKTOP-POSS0RQ\olega <olegackiv956@gmail.com>
2022-11-11 10:33:28 +0200

    Added result chain output

--- Lab 4/Lab 4.py
+++ Lab 4/Lab 4.py
@@ -1,9 +1,11 @@
 score: list[int] = []
 edges: list[list[int]] = []
-chain: list[str] = []
+chains: list[list[str]] = []
 
-def get_longest_chain(size: int, words: list[str]) -> int:
+
+def get_longest_chain(size: int, words: list[str]) -> tuple[int, list[str]]:
     length = 0
+    result_index = 0
     position: dict[str, int] = {}
 
     i = 0
@@ -22,17 +24,32 @@ def get_longest_chain(size: int, words: list[str]) -> int:
         i += 1
 
     for i in range(size):
-        length = max(length, get_length(i))
+        chains.append([])
+        curr_lenth = get_length(i, i, words)
+        if curr_lenth > length:
+            length = curr_lenth
+            result_index = i
 
-    return length
+    return length, chains[result_index]
 
-def get_length(vertex: int) -> int:
+
+def get_length(vertex: int, index, words: list[str]) -> int:
+    if not chain_has_of_length(index, words[vertex].__len__()):
+        chains[index].append(words[vertex])
     if score[vertex] > 0:
         return score[vertex]
     score[vertex] = 1
     for dest in edges[vertex]:
-        score[vertex] = max(score[vertex], get_length(dest) + 1)
+        score[vertex] = max(score[vertex], get_length(dest, index, words) + 1)
     return score[vertex]
+
+
+def chain_has_of_length(index, len):
+    for word in chains[index]:
+        if word.__len__() == len:
+            return True
+    return False
+
 
 def main():
     size = 0
@@ -45,7 +62,8 @@ def main():
 
     print(f"Size: {size}")
     print(f"All words: {words}")
-    length = get_longest_chain(size, words)
+    length, chain = get_longest_chain(size, words)
+    print(f"Chain: {chain}")
     print(f"Length: {length}")
 
     with open('Lab 4/wchain.out', "a") as writer:
